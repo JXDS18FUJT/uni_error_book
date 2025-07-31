12cc88bb0287a7c2c1d2c6594a04c05856ae175d
JXDS18FUJT <347613781@qq.com>
2025-07-31 17:58:15 +0800

    123

--- src/pages/home/home.vue
+++ src/pages/home/home.vue
@@ -1,15 +1,6 @@
 <template>
 	<view>
-		<uni-forms ref="form" :modelValue="formData">
-			<uni-forms-item name="username" label="用户名">
-				<uni-easyinput v-model="formData.username" type="text" placeholder="请输入用户名" />
-			</uni-forms-item>
-			<uni-forms-item name="password" label="密码">
-				<uni-easyinput v-model="formData.password" type="text" placeholder="请输入密码" />
-			</uni-forms-item>
-			<button class="button" @click="submit">登录</button>
-		<navigator url="/pages/login/login">前往登录</navigator>
-		</uni-forms>
+		
 	</view>
 
 </template>
@@ -77,7 +68,7 @@
 				// 			dataType:'json',
 				// 			header:{
 				// 				'Authorization':res.data.data.token
-								
+
 				// 			},
 				// 			success(resu) {
 				// 				console.log(resu)
